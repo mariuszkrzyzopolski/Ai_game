7ccc09a2dc5d76a36f1889360acca0f28bd6e5e7
warhir <krzyzopolski.mariusz@gmail.com>
2022-10-16 16:48:06 +0200

    add TODO with idea of features

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ class SixMensMorris(TwoPlayerGame):
         self.player_1_pieces, self.player_2_pieces = 6, 6
         self.board = [0 for i in range(16)]
         self.current_player = 1  # player 1 starts.
+        self.nmove = 0
 
     def possible_moves(self):
         return [i + 1 for i, e in enumerate(self.board) if e == 0]
 
     def make_move(self, move):
-        print(self.possible_moves())
-        print([self.player_1_pieces, self.player_2_pieces])
         self.board[int(move) - 1] = self.current_player
         self.pound()
+        # TODO better movement, for now after place 6 pieces game stat to get pieces from board, begins from min
+        if self.nmove == 12:
+            self.board[next(x for x, val in enumerate(self.board) if val == self.current_player)] = 0
 
     def pound(self):
         mill = any(
@@ -36,21 +38,27 @@ class SixMensMorris(TwoPlayerGame):
             ]
             ]
         )
+        # TODO we need to detect old mill, to get out from loop
         if mill:
             if self.current_player == 1:
-                self.player_1_pieces = self.player_1_pieces - 1
-            else:
                 self.player_2_pieces = self.player_2_pieces - 1
-            print(f"{self.player_1_pieces} {self.player_2_pieces}")
+                self.board[next(x for x, val in enumerate(self.board) if val == 2)] = 0
+            else:
+                self.player_1_pieces = self.player_1_pieces - 1
+                self.board[next(x for x, val in enumerate(self.board) if val == 1)] = 0
 
     def lose(self):
         if self.player_1_pieces < 3 or self.player_2_pieces < 3:
             return True
 
     def is_over(self):
+        # TODO winner message?
         return (self.possible_moves() == []) or self.lose()
 
     def show(self):
+        # TODO new board
+        print(self.nmove)
+        print([self.player_1_pieces, self.player_2_pieces])
         print(self.board)
 
     def scoring(self):
