b939e49c3f0b784c9aee9902bf2c5e1f1cd30748
warhir <krzyzopolski.mariusz@gmail.com>
2022-12-01 21:42:09 +0100

    complete documentation

--- Ionosphere/top500.py
+++ Ionosphere/top500.py
@@ -1,3 +1,7 @@
+"""
+Authors: Mariusz Krzyzopolski s21544 Tomasz Baj s20499
+"""
+
 from sklearn import svm
 import matplotlib.pyplot as plt
 from sklearn import model_selection
@@ -5,18 +9,22 @@ from sklearn import tree
 import pandas as pd
 import numpy as np
 
+""" Read xlsx file and drop rows with NaN values for Year, Efficiency or Power consumption """
 exel = pd.read_excel('TOP500_202211.xlsx', index_col=None, header=0)
 df = exel.iloc[:, [0, 9, 13, 18]]
 df = df.dropna()
 df = df.reset_index(drop=True)
 
+""" Divide data to training and test dataset"""
 X = df.iloc[:, [2, 3]]
 Y = df.iloc[:, 1]
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, Y, random_state=0)
 
+""" Train and predict with classifier"""
 svc = svm.SVC(kernel='rbf', C=1).fit(X_train, y_train)
 Z = svc.predict(X_test)
 
+""" Plot with classification divided for each year from test dataset """
 plt.scatter(X_test.iloc[Z < 2018, 1], X_test.iloc[Z < 2018, 0], c='black', marker='o', s=50, label="older")
 plt.scatter(X_test.iloc[Z == 2018, 1], X_test.iloc[Z == 2018, 0], c='orange', marker='o', s=50, label="2018")
 plt.scatter(X_test.iloc[Z == 2019, 1], X_test.iloc[Z == 2019, 0], c='blue', marker='o', s=50, label="2019")
@@ -32,10 +40,13 @@ plt.show()
 
 print("Accuracy on SVC model: ", svc.score(X_test, y_test))
 
+
+""" Create decision tree and train it with X and y """
 dec_tree = tree.DecisionTreeClassifier().fit(X_train, y_train)
 print(f"Score for train test in tree model: {dec_tree.score(X_train, y_train)}")
 print(f"Score for test set in tree model: {dec_tree.score(X_test, y_test)}")
 
+""" Plot tree with each unique year from dataset """
 class_name = np.unique(list(map(str, df['Year'].tolist())))
 feature = [str(i) for i in X.columns]
 plt.figure()
